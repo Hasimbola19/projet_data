83ca02724bfe299237a9743af3dbf7db20d11eec
aliciaagdl <165413117+aliciaagdl@users.noreply.github.com>
2025-10-20 09:57:23 +0200

    Update models.py

--- portefeuille/models.py
+++ portefeuille/models.py
@@ -1,8 +1,10 @@
+# contient les modeles de bdd
 from django.db import models
 
+#type d'actif
 class Actif(models.Model):
     nom = models.CharField(max_length=50)
-
+#etf avec un nom et un code unique ole ticker
 class ETF(models.Model):
     nom = models.CharField(max_length=100)
     ticker = models.CharField(max_length=10, unique=True)
